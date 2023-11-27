3f0c941d5d86dca1f6abacd4931f6bad14b983be
Schlenge <emil.tarala@gmail.com>
2023-11-27 15:50:13 +0200

    commit 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,38 @@
-import { Routes, Route } from 'react-router-dom';
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import css from '../components/App.module.css';
 
+import Feedback from './Feedback/Feedback';
+import ImageFinder from './Image-finder/ImageFinder';
+import Phonebook from './Phonebook/Phonebook';
+
 export const App = () => {
-  return <div className={css.Title}>React Homework 4</div>;
+  return (
+    <div>
+      <h1 className={css.Title}>HW 4 - React hooks</h1>
+      <nav className={css.Navbar}>
+        <ul>
+          <li className={css.MenuItem}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={css.MenuItem}>
+            <Link to="/Feedback">Feedback</Link>
+          </li>
+          <li className={css.MenuItem}>
+            <Link to="/ImageFinder">Image-Finder</Link>
+          </li>
+          <li className={css.MenuItem}>
+            <Link to="/Phonebook">Phonebook</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={css.Content}>
+        <Routes>
+          <Route path="/Feedback" element={<Feedback />} />
+          <Route path="/ImageFinder" element={<ImageFinder />} />
+          <Route path="/Phonebook" element={<Phonebook />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
